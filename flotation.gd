874575640eb1f94028a9e7bf99b31445443df705
Nick Francisci <nickfrancisci@gmail.com>
2025-04-20 04:48:35 -0400

    update params

--- flotation.gd
+++ flotation.gd
@@ -28,25 +28,27 @@ func _physics_process(delta: float) -> void:
 		# Depth is negative.
 		# Buoyancy is only linearly proportional to depth, but for each unit one
 		# corner sinks, it's submerging 
-		var spring_force = buoyancy_spring * - depth
+		var spring_force = Vector3(0, buoyancy_spring * - depth, 0)
 		
 		# Calculate damping force (proportional to vertical velocity) to account for having
 		# to push water out of the way or pull it in
 		# Not sure that this is correct
-		# TODO: should damp all velocity AT POINT (ie. angular * moment arm = velocity at point)
+		# should damp all velocity AT POINT (ie. angular * moment arm = velocity at point)
 		var point_velocity = linear_velocity + angular_velocity.cross(mesh_inst.to_local(point) - center_of_mass);
 		
 		var hydrodynamic_drag = Vector3.ZERO
 		if depth < 0:
 			hydrodynamic_drag = - buoyancy_damping * point_velocity * point_velocity * point_velocity
 		#print("point " + str(point) + ", depth: " + str(depth))
+		#print("drag " + str(hydrodynamic_drag))
+		#print("spring" + str(spring_force))
+		#print("point" + str(point))		
 		
 		# Combine forces and clamp to avoid excessive force
-		var raw_force = (Vector3(0, spring_force, 0) + hydrodynamic_drag) / points_dict.size()
+		var raw_force = (spring_force + hydrodynamic_drag) / points_dict.size()
 		var scaled_force = raw_force * delta
 		 
-		#buoyancy_force = clamp(buoyancy_force, -buoyancy_max_force, buoyancy_max_force)
-		# Apply the force at the marker's position
-		#var force = Vector3(0, buoyancy_force, 0) / points_dict.size() # more sampling points does not mean more buoyancy
+		# The Godot docs for the position are incorrect: the point should be in 
+		# local coordinates to the body
 		apply_impulse(scaled_force, mesh_inst.to_local(point))
 		
